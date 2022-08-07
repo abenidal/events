a352e476bbfa43ecf3d4679df933fe75472f34d7
abenidal <benidal.afaf@gmail.com>
2022-08-07 22:55:01 +0100

    Update project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Events</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <h1>Event For Good</h1>
   <router-view />
 </template>
 
@@ -26,5 +27,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 14px;
 }
 </style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 111111,
+          category: "Category",
+          title: "Cat Adoption Day",
+          description: "Find your",
+          location: "Meaow Town",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Afaf",
+        },
+        {
+          id: 222222,
+          category: "Category",
+          title: "Cat Adoption Day",
+          description: "Find your",
+          location: "Meaow Town",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Afaf",
+        },
+        {
+          id: 11133333111,
+          category: "Category",
+          title: "Cat Adoption Day",
+          description: "Find your",
+          location: "Meaow Town",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Afaf",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
